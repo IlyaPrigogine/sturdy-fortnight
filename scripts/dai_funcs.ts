2b208f3ce97f241bf27caf8be87e6efc3722b884
IlyaPrigogine <prigogine@protonmail.com>
2021-12-02 06:23:22 +0800

    Update dai_funcs.ts

--- scripts/dai_funcs.ts
+++ scripts/dai_funcs.ts
@@ -7,12 +7,12 @@ const {execute, read} = deployments;
 async function main() {
     const {owner,user1} = await getNamedAccounts();
 
-    const MockToken = await ethers.getContract<MockToken>('MockToken');
+    // const MockToken = await ethers.getContract<MockToken>('MockToken');
     const MyDefiProject = await ethers.getContract<MyDefiProject>('MyDefiProject');
 
-    await MockToken.approve(MyDefiProject.address, parseEther("10000"));
+    // await MockToken.approve(MyDefiProject.address, parseEther("10000"));
     // console.log(`Before: MockToken.balanceOf(user1): ${formatEther(await MockToken.balanceOf(user1))}`);
-    // await MyDefiProject.foo(user1,parseEther("1.35"));
+    await MyDefiProject.foo(ethers.constants.AddressZero,parseEther("1.35"));
     // console.log(`After: MockToken.balanceOf(user1): ${formatEther(await MockToken.balanceOf(user1))}`);
 }
 
