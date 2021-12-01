0b67cdc0a23852ec461e8305fcd84532e82dead0
IlyaPrigogine <prigogine@protonmail.com>
2021-12-02 06:52:56 +0800

    submitted

--- scripts/dai_funcs.ts
+++ scripts/dai_funcs.ts
@@ -5,15 +5,18 @@ import {formatEther, parseEther} from "ethers/lib/utils";
 const {execute, read} = deployments;
 
 async function main() {
-    const {owner,user1} = await getNamedAccounts();
+    const {owner} = await getNamedAccounts();
 
-    // const MockToken = await ethers.getContract<MockToken>('MockToken');
+    // const MockToken = await ethers.getContract<MockToken>('MockToken');  -- localhost
+    const MockToken = await ethers.getContractAt(
+        'IERC20',
+        '0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa'
+    );//kovan dai address
+
     const MyDefiProject = await ethers.getContract<MyDefiProject>('MyDefiProject');
-
+    console.log(`${formatEther(await MockToken.balanceOf(owner))}`);
     // await MockToken.approve(MyDefiProject.address, parseEther("10000"));
-    // console.log(`Before: MockToken.balanceOf(user1): ${formatEther(await MockToken.balanceOf(user1))}`);
-    await MyDefiProject.foo(ethers.constants.AddressZero,parseEther("1.35"));
-    // console.log(`After: MockToken.balanceOf(user1): ${formatEther(await MockToken.balanceOf(user1))}`);
+    await MyDefiProject.foo(ethers.constants.AddressZero,parseEther("1.123456789"));
 }
 
 
